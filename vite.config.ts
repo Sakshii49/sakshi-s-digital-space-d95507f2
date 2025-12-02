2a42753b61600b3289bb78e4d54b18dfd6d6d4a1
sakshi khatavkar <140887464+Sakshii49@users.noreply.github.com>
2025-12-02 15:36:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+base: '/sakshi-s-digital-space-d95507f2/'
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
